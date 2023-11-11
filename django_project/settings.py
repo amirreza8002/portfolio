66c712acb7446d7181afe6df084fd971bec73b4f
amirreza <amir.rsf1380@gmail.com>
2023-11-11 19:49:10 +0330

    added the tick icon for done todos

--- django_project/settings.py
+++ django_project/settings.py
@@ -183,6 +183,11 @@ STORAGES = {
     },
 }
 
+# media
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
